f7c7e359ea571061f0be39bdf99ed290619a0679
Siyaun Hua <siyuan_hua@apple.com>
2017-06-13 15:19:58 -0700

    add more useful things

--- debian.sh
+++ debian.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-
+# setup the fish shell
 sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get install fish
 
 curl -L https://get.oh-my.fish | fish
+
+# set the default shell to fish
+chsh -s `which fish`
+
+# setup the vim environment
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
